cb74ea86a84a79f4445e831bac50fe1f825fe166
GerardoGomezG <ing.gerardogomez@gmail.com>
2023-05-22 21:29:34 -0400

    Adds header and reload icon

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
+      template: './src/index.html',
+      filename: './index.html',
     }),
   ],
   output: {
