36093d696f6d9b8c805c830733d7aa19593abb1c
GerardoGomezG <ing.gerardogomez@gmail.com>
2023-05-23 17:46:53 -0400

    Creates project structure: 1st part

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './style.css';
 import './index.html';
+
+const checkbox = document.querySelectorAll('input[type=checkbox]');
+// let checked = document.querySelectorAll('input:checked');
+
+checkbox.forEach((a) => {
+  a.addEventListener('click', (e) => {
+    e.target.parentElement.classList.toggle('textLineThrough');
+  });
+});
+
+// if (checked.length === 0) {
+// console.log('No checkboxes checked');
+// } else {
+// console.log(`${checked.length}, checkboxes checked`);
+// }
