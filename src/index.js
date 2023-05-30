99f2c6d8796a3d0d203ef5984a9565694c1e13e8
GerardoGomezG <ing.gerardogomez@gmail.com>
2023-05-29 20:14:13 -0400

    Enables task adding to list Adds ClearAllCompletedTask function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
 import './styles.css';
 import './index.html';
+import verticalDots from './assets/verticalDots.svg';
+// import trash from './assets/trash.svg';
 
-const checkbox = document.querySelectorAll('input[type=checkbox]');
-// let checked = document.querySelectorAll('input:checked');
+// const task = [];
+const tasksList = document.querySelector('#tasksList');
+const newTaskInput = document.querySelector('#newTaskInput');
+// let liElements = document.querySelectorAll('li');
+let checkbox;
+const clearAllCompleted = document.querySelector('.h3Container');
 
-checkbox.forEach((a) => {
-  a.addEventListener('click', (e) => {
-    e.target.parentElement.classList.toggle('textLineThrough');
+function addTask(description, checkStatus) {
+  // const index = task.length;
+  const li = document.createElement('li');
+  const cb = document.createElement('input');
+  const span = document.createElement('span');
+  const image = document.createElement('img');
+  // const hr = document.createElement('hr');
+  cb.setAttribute('type', 'checkbox');
+  cb.checked = checkStatus;
+  span.textContent = description;
+  image.src = verticalDots;
+  image.alt = 'dots';
+  li.prepend(cb);
+  li.appendChild(span);
+  li.appendChild(image);
+  tasksList.appendChild(li);
+  // tasksList.appendChild(hr);
+  checkbox = document.querySelectorAll('input[type=checkbox]');
+  // liElements = document.querySelectorAll('li');
+  // task[index] = {
+  // description,
+  // completed: checkStatus,
+  // index,
+  // };
+  // localStorage.setItem(index, JSON.stringify(task[index]));
+}
+
+// function editTask(x) {
+// console.log('Entra');
+// }
+function clearCompleted() {
+  checkbox.forEach((element) => {
+    if (element.checked) {
+      element.parentElement.remove(); // Deletes the selected tasks
+    }
   });
+}
+
+newTaskInput.addEventListener('keydown', (e) => {
+  const newTask = e.target.value.trim();
+  if ((e.key === 'Enter' || e.key === 'NumpadEnter') && (newTask.length > 0)) {
+    addTask(newTask, false);
+    e.target.value = '';
+  }
 });
 
-// if (checked.length === 0) {
-// console.log('No checkboxes checked');
-// } else {
-// console.log(`${checked.length}, checkboxes checked`);
-// }
+// liElements.forEach((a) => {
+// a.addEventListener('click', () => editTask(a));
+// });
+
+clearAllCompleted.addEventListener('click', () => clearCompleted());
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Cambiar la lógica del Local Storage porque se puede estar almacenando otras cosas
+  if (localStorage.length === 0) {
+    addTask('Wash the dogs', false);
+    addTask('Complete To Do list project', false);
+    addTask('fix car', false);
+  }
+});
