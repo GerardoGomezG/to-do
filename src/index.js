bd62b350b8fc583c415c379825ec4cea046bb946
GerardoGomezG <ing.gerardogomez@gmail.com>
2023-06-02 11:14:47 -0400

    Saves new task in LocalStorage

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import './index.html';
 import verticalDots from './assets/verticalDots.svg';
 import trash from './assets/trash.svg';
 
-// const task = [];
+const savedTasks = [];
 const tasksList = document.querySelector('#tasksList');
 const newTaskInput = document.querySelector('#newTaskInput');
 let liElements = document.querySelectorAll('li');
 let checkbox = document.querySelectorAll('.cb');
-// let textarea = document.querySelectorAll('.ta');
 const clearAllCompleted = document.querySelector('.h3Container');
 
 function addTask(description, checkStatus) {
@@ -16,6 +15,7 @@ function addTask(description, checkStatus) {
   const cb = document.createElement('input');
   const input = document.createElement('input');
   const image = document.createElement('img');
+  const task = [];
   cb.setAttribute('type', 'checkbox');
   cb.checked = checkStatus;
   cb.classList.add('cb');
@@ -23,26 +23,25 @@ function addTask(description, checkStatus) {
   input.value = description;
   input.classList.add('ta');
   image.src = verticalDots;
-  image.alt = 'dots';
+  image.alt = 'menu';
   li.prepend(cb);
   li.appendChild(input);
   li.appendChild(image);
   tasksList.appendChild(li);
   checkbox = document.querySelectorAll('.cb');
-  // textarea = document.querySelectorAll('.ta');
   liElements = document.querySelectorAll('li');
-  // task[index] = {
-  // description,
-  // completed: checkStatus,
-  // index,
-  // };
-  // localStorage.setItem(index, JSON.stringify(task[index]));
+  // Task saving in an array element
+  task.push({ description, completed: checkStatus, index: savedTasks.length });
+  savedTasks.push(task);
+  // Local-Storage saving
+  localStorage.setItem('savedTasks', JSON.stringify(savedTasks));
 }
 
 function editTask(x) {
   x.parentElement.classList.add('yellow_li');
+  x.classList.add('yellow_li');
   x.parentElement.querySelector('img').setAttribute('src', trash);
-  x.parentElement.querySelector('img').setAttribute('alt', 'trash');
+  x.parentElement.querySelector('img').setAttribute('alt', 'delete');
 }
 
 function clearCompleted() {
@@ -64,17 +63,18 @@ newTaskInput.addEventListener('keydown', (e) => {
 clearAllCompleted.addEventListener('click', () => clearCompleted());
 
 document.addEventListener('click', (e) => {
-  if (e.target.alt === 'trash') {
+  if (e.target.alt === 'delete') {
     e.target.parentElement.remove();
   }
   liElements.forEach((element) => {
     if (element.matches('.yellow_li')) {
       element.classList.remove('yellow_li');
+      element.querySelector('.ta').classList.remove('yellow_li');
       element.querySelector('img').setAttribute('src', verticalDots);
-      element.querySelector('img').setAttribute('alt', 'dots');
+      element.querySelector('img').setAttribute('alt', 'menu');
     }
   });
-  if (document.activeElement.matches('.ta') || (e.target.alt === 'dots')) {
+  if (document.activeElement.matches('.ta') || (e.target.alt === 'menu')) {
     // Focuses on textarea input if verticalDots img is clicked
     e.target.parentElement.querySelector('.ta').focus();
     editTask(e.target);
